be21e8f274fd29b18c9bcc8e3511cfb64133bad2
KatsiarynaDunets <kate.dunets@gmail.com>
2023-10-12 00:10:09 +0200

    Version 1.0

---
+++ RestApp/Controllers/ActionCVCell.swift
@@ -0,0 +1,14 @@
+//
+//  ActionCVCell.swift
+//  RestApp
+//
+//  Created by Kate on 11/10/2023.
+//
+
+import UIKit
+
+import UIKit
+
+class ActionCVCell: UICollectionViewCell {
+    @IBOutlet weak var infiLbl: UILabel!
+}
---
+++ RestApp/Models/ApiConstants.swift
@@ -0,0 +1,28 @@
+//
+//  ApiConstants.swift
+//  RestApp
+//
+//  Created by Kate on 11/10/2023.
+//
+
+import Foundation
+
+import Foundation
+
+struct ApiConstants {
+    
+    /// Local server path
+    static let serverPath = "http://localhost:3000/"
+    /// Users
+    static let usersPath = serverPath + "users"
+    static let usersURL = URL(string: usersPath)
+    
+    /// Posts
+    
+    /// ToDo-s
+    
+    /// Alboms
+    
+    /// Photos
+    
+}
